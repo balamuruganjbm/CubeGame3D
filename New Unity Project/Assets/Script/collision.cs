33047193da5896a5a54ae27278b98802a831bbca
balamuruganjbm <32296343+balamuruganjbm@users.noreply.github.com>
2018-05-17 13:36:23 +0530

    first commit

---
+++ New Unity Project/Assets/Script/collision.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class collision : MonoBehaviour {
+	public first move;
+
+	void OnCollisionEnter(Collision colinfo)
+	{
+		if (colinfo.collider.tag == "obs") 
+		{
+			move.enabled = false;
+		}
+		
+	}
+}
---
+++ New Unity Project/Assets/Script/first.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class first : MonoBehaviour {
+
+	// Use this for initialization
+	public Rigidbody rb; 
+	public float fforce = 2000f;
+	public float sforce = 500f;
+	// Update is called once per frame
+	void FixedUpdate () {
+		rb.AddForce (0, 0, fforce * Time.deltaTime);
+		if (Input.GetKey ("d")) 
+		{
+			rb.AddForce (sforce * Time.deltaTime, 0, 0,ForceMode.VelocityChange);
+		}
+		if (Input.GetKey ("a")) 
+		{
+			rb.AddForce (-sforce * Time.deltaTime,0, 0,ForceMode.VelocityChange);
+		}
+	}
+}
